88fcff0fcd9dfc0a8bed0cf92b60509f1684f1d4
Vasiliy Rozhkov <profilrv@gmail.com>
2023-10-07 19:29:40 +0500

    refactor(painting): Прописал extended, чтобы явно указать наследников какого класса ожидает джинерик

--- src/main/java/com/example/paint/interactors/painting/Draw.java
+++ src/main/java/com/example/paint/interactors/painting/Draw.java
@@ -3,6 +3,6 @@ package com.example.paint.interactors.painting;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Shape;
 
-public abstract class Draw<MyShape> {
+public abstract class Draw<MyShape extends com.example.paint.interactors.shapes.Shape> {
     abstract Shape draw(Pane pane, MyShape shape, Brush brush);
 }

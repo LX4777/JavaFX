5f039bd99f6e68ed83c3071d0b76b59a142f5447
Vasiliy Rozhkov <profilrv@gmail.com>
2023-10-14 12:33:24 +0500

    feat(PaintController): Морфинг полигонов

--- src/main/java/com/example/paint/interactors/morphing/NewMorph.java
+++ src/main/java/com/example/paint/interactors/morphing/NewMorph.java
@@ -3,8 +3,6 @@ package com.example.paint.interactors.morphing;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.geometry.Point2D;
-import javafx.scene.Group;
-import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
@@ -15,67 +13,64 @@ import java.util.List;
 
 public class NewMorph {
     private List<Polygon> interpolatedPolygons = new ArrayList<>();
-
     private int numSteps = 100; // количество этапов преобразования
-//    private int desiredStep = 100; // номер этапа, который хотим увидеть
 
-    public void make(Pane root, Polygon startPolygon, Polygon endPolygon, int desiredStep){
+    public void make(Pane root, Polygon startPolygon, Polygon endPolygon, int desiredStep) {
         interpolatePoints(startPolygon, endPolygon);
-
         Polygon polygon = new Polygon();
         polygon.setFill(Color.TRANSPARENT);
         polygon.setStroke(Color.BLACK);
+        root.getChildren().add(polygon);
 
         Timeline timeline = new Timeline();
         for (int step = 0; step < numSteps; step++) {
             Polygon currentPolygon = interpolatedPolygons.get(step);
-            polygon.getPoints().addAll(currentPolygon.getPoints());
-
+            if (step == desiredStep) {
+                polygon.getPoints().setAll(currentPolygon.getPoints());
+            }
             int currentStep = step; // создаем копию переменной для использования внутри лямбда-выражения
             KeyFrame keyFrame = new KeyFrame(Duration.millis(10000 * step), event -> {
                 if (currentStep == desiredStep) {
                     System.out.println("Этап " + currentStep + ": " + interpolatedPolygons.get(currentStep).getPoints());
                 }
             });
-
             timeline.getKeyFrames().add(keyFrame);
         }
 
-        root.getChildren().add(polygon);
-
         timeline.play();
     }
+
     private void interpolatePoints(Polygon startPolygon, Polygon endPolygon) {
-        int maxSize = Math.max(startPolygon.getPoints().size() / 2, endPolygon.getPoints().size() / 2);
+        int startSize = startPolygon.getPoints().size() / 2;
+        int endSize = endPolygon.getPoints().size() / 2;
         List<Point2D> startPoints = getPolygonPoints(startPolygon);
         List<Point2D> endPoints = getPolygonPoints(endPolygon);
+
+        double[] startX = new double[startSize];
+        double[] startY = new double[startSize];
+        double[] endX = new double[endSize];
+        double[] endY = new double[endSize];
+
+        for (int i = 0; i < startSize; i++) {
+            startX[i] = startPoints.get(i).getX();
+            startY[i] = startPoints.get(i).getY();
+        }
+
+        for (int i = 0; i < endSize; i++) {
+            endX[i] = endPoints.get(i).getX();
+            endY[i] = endPoints.get(i).getY();
+        }
 
         for (int step = 0; step < numSteps; step++) {
             double t = (double) step / (numSteps - 1); // интерполяционный параметр от 0 до 1
             Polygon interpolated = new Polygon();
-
-            for (int i = 0; i < maxSize; i++) {
-                Point2D startPoint = i < startPoints.size() ? startPoints.get(i) : new Point2D(0, 0);
-                Point2D endPoint = i < endPoints.size() ? endPoints.get(i) : new Point2D(0, 0);
-                double x = (1 - t) * startPoint.getX() + t * endPoint.getX(); // интерполяция координаты x
-                double y = (1 - t) * startPoint.getY() + t * endPoint.getY(); // интерполяция координаты y
+            for (int i = 0; i < Math.max(startSize, endSize); i++) {
+                double x = (1 - t) * (i < startSize ? startX[i] : 0) + t * (i < endSize ? endX[i] : 0); // интерполяция координаты x
+                double y = (1 - t) * (i < startSize ? startY[i] : 0) + t * (i < endSize ? endY[i] : 0); // интерполяция координаты y
                 interpolated.getPoints().addAll(x, y);
             }
-
             interpolatedPolygons.add(interpolated);
         }
-    }
-
-    private Polygon createStartPolygon() {
-        Polygon polygon = new Polygon();
-        polygon.getPoints().addAll(0.0, 0.0, 2.0, 0.0, 2.0, 2.0, 0.0, 2.0);
-        return polygon;
-    }
-
-    private Polygon createEndPolygon() {
-        Polygon polygon = new Polygon();
-        polygon.getPoints().addAll(1.0, 1.0, 3.0, 1.0, 3.0, 3.0, 1.0, 3.0);
-        return polygon;
     }
 
     private List<Point2D> getPolygonPoints(Polygon polygon) {
